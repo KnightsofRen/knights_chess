01692dc21132ddfb18446c799158fc84d7fe1fa7
Skjamal <satinder.jamal@gmail.com>
2016-05-22 14:19:28 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KnightsChess::Application.config.secret_key_base = '9edea7ad3ea57cd4e271332a09b095834ea836c19453ecb2854150764c88053816fee3b4ea6af2e4f03113ecf1f0d493728fbd4daecc10c6d7bfd13f2188a543'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KnightsChess::Application.config.session_store :cookie_store, key: '_knights_chess_session'
