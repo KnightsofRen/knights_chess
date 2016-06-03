4f9fd4b1bf69aaf41ce1c65988a460110bd2d48f
Brandon Gorman <doyouthunderstand@gmail.com>
2016-06-03 03:35:28 +0000

    Added scope and added available games to index, but scope is not working yet

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
-  enum status: [:safe, :check, :checkmate]
+    scope :available, -> {where((@games.player_white_id == nil) || (@games.player_black_id == nil)) }
+    enum status: [:safe, :check, :checkmate]
 end
