4c42c32f87e7d7b12351b5d166be1bed491bec51
jovanka <jovanka.placeres@gmail.com>
2016-06-08 04:47:48 +0000

    rubocop edits

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,18 +1,18 @@
 class King < Piece
-  #valid moves--
-  #king may move one square any direction
-  #must NOT move onto square that is being attacked by opposing pieces
-  #may capture piece IF where it's moving to there is no other piece protecting soon-to-be-captured piece(in-check)
-  #because king may NOT move onto square that is being attacked 2 kings may NEVER stand next to each other => position: illegal
+  # valid moves--
+  # king may move one square any direction
+  # must NOT move onto square that is being attacked by opposing pieces
+  # may capture piece IF where it's moving to there is no other piece protecting soon-to-be-captured piece(in-check)
+  # because king may NOT move onto square that is being attacked 2 kings may NEVER stand next to each other => position: illegal
 
-  def valid_move?(x,y)
-    return false unless position_on_board?(x,y)
-    return false if move_too_far?(x,y)
-    return false if move_into_position_under_attack?(x,y)
-    return true
+  def valid_move?(x, y)
+    return false unless position_on_board?(x, y)
+    return false if move_too_far?(x, y)
+    return false if move_into_position_under_attack?(x, y)
+    true
   end
 
-  def position_on_board?(x,y)
+  def position_on_board?(x, y)
     return false if x >= 8
     return false if x < 0
     return false if y < 0
@@ -20,14 +20,13 @@ class King < Piece
     true
   end
 
-  def move_too_far?(x,y)
+  def move_too_far?(x, y)
     return true if (x_coordinate - x).abs > 1
     return true if (y_coordinate - y).abs > 1
     false
   end
 
-  def move_into_position_under_attack?(x,y)
-    false #need each opponent pieces to implement?
+  def move_into_position_under_attack?(_x, _y)
+    false # need each opponent pieces to implement?
   end
 end
-
