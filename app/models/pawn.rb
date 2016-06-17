9e9db3954b5b91ac56f11c40d8c7a6e08633b3e5
Anthony Sin <anthony.t.sin@gmail.com>
2016-06-17 22:14:09 +0000

    rubocop corr

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -6,42 +6,35 @@ class Pawn < Piece
 
   def valid_move?(x, y)
     return false if obstructed?(x, y) == 'Error: invalid input'
-    return true if first_move?(x,y) unless obstructed?(x,y)
+    return true unless obstructed?(x, y) || !first_move?(x, y)
     return false if backward_sideway_or_over_one_move?(x, y)
-    return false if piece_present_at_target_vertical_destination?(x,y)
-    return false if piece_not_present_at_target_diagonal_destination?(x,y)
+    return false if piece_present_at_target_vertical_destination?(x, y)
+    return false if piece_not_present_at_target_diagonal_destination?(x, y)
     true
   end
 
   private
 
   def first_move?(x, y)
-    return false if piece_present_at_target_vertical_destination?(x,y)
-    if color == 'white'
-      return true if y == (y_coordinate + 2) && y_coordinate == 1 && x_coordinate == x 
-    else
-      return true if y == (y_coordinate - 2) && y_coordinate == 6 && x_coordinate == x
-    end
+    return false if piece_present_at_target_vertical_destination?(x, y)
+    return true if color == 'white' && y == (y_coordinate + 2) && y_coordinate == 1 && x_coordinate == x
+    return true if color == 'black' && y == (y_coordinate - 2) && y_coordinate == 6 && x_coordinate == x
     false
   end
 
-  def backward_sideway_or_over_one_move?(x, y)
-    if color == 'white'
-      return false if (y - y_coordinate) == 1
-    else
-      return false if (y - y_coordinate) == -1 
-    end 
+  def backward_sideway_or_over_one_move?(_x, y)
+    return false if color == 'white' && (y - y_coordinate) == 1
+    return false if color == 'black' && (y - y_coordinate) == -1
     true
   end
 
-  def piece_present_at_target_vertical_destination?(x,y)
+  def piece_present_at_target_vertical_destination?(x, y)
     return true if x_coordinate == x && Game.find(game_id).pieces.find_by(x_coordinate: x, y_coordinate: y).present?
     false
   end
 
-  def piece_not_present_at_target_diagonal_destination?(x,y)
+  def piece_not_present_at_target_diagonal_destination?(x, y)
     return true if x_coordinate != x && Game.find(game_id).pieces.find_by(x_coordinate: x, y_coordinate: y).nil?
     false
   end
-
 end
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Pawn, type: :model do
       game.pieces.delete_all
 
       # white pawn at (0, 1) ==> 2 valid moves
-      pawn = FactoryGirl.create(:pawn ,game_id: game.id)
+      pawn = FactoryGirl.create(:pawn, game_id: game.id)
 
       # black pawn2 at (2, 6), piece at (2, 5) ==> 0 valid moves
       pawn2 = FactoryGirl.create(:pawn, color: 'black', x_coordinate: 2, y_coordinate: 6, game_id: game.id)
@@ -16,14 +16,14 @@ RSpec.describe Pawn, type: :model do
       # black pawn3 at (4, 6), piece at (4, 4) ==> 1 valid move
       pawn3 = FactoryGirl.create(:pawn, color: 'black', x_coordinate: 4, y_coordinate: 6, game_id: game.id)
       FactoryGirl.create(:piece, x_coordinate: 4, y_coordinate: 4, game_id: game.id)
-      
+
       # white pawn4 at (6, 1), pieces at (5, 2) and (7, 2) ==> 4 valid moves
-      pawn4 = FactoryGirl.create(:pawn ,x_coordinate: 6, y_coordinate: 1, game_id: game.id)
+      pawn4 = FactoryGirl.create(:pawn, x_coordinate: 6, y_coordinate: 1, game_id: game.id)
       FactoryGirl.create(:piece, x_coordinate: 5, y_coordinate: 2, game_id: game.id)
       FactoryGirl.create(:piece, x_coordinate: 7, y_coordinate: 2, game_id: game.id)
 
       # white pawn5 at (6, 4), piece (7, 5) ==> 2 valid moves
-      pawn5 = FactoryGirl.create(:pawn ,x_coordinate: 6, y_coordinate: 4, game_id: game.id)
+      pawn5 = FactoryGirl.create(:pawn, x_coordinate: 6, y_coordinate: 4, game_id: game.id)
       FactoryGirl.create(:piece, x_coordinate: 7, y_coordinate: 5, game_id: game.id)
 
       test = 0
@@ -38,13 +38,13 @@ RSpec.describe Pawn, type: :model do
       end
       expect(test).to eq 9
 
-      expect(pawn.valid_move?(0,2)).to eq true
-      expect(pawn.valid_move?(0,3)).to eq true
-      expect(pawn3.valid_move?(4,5)).to eq true
-      expect(pawn3.valid_move?(4,4)).to eq false
-      expect(pawn4.valid_move?(5,2)).to eq true
-      expect(pawn4.valid_move?(6,3)).to eq true
-      expect(pawn5.valid_move?(5,5)).to eq false
+      expect(pawn.valid_move?(0, 2)).to eq true
+      expect(pawn.valid_move?(0, 3)).to eq true
+      expect(pawn3.valid_move?(4, 5)).to eq true
+      expect(pawn3.valid_move?(4, 4)).to eq false
+      expect(pawn4.valid_move?(5, 2)).to eq true
+      expect(pawn4.valid_move?(6, 3)).to eq true
+      expect(pawn5.valid_move?(5, 5)).to eq false
     end
   end
 end
